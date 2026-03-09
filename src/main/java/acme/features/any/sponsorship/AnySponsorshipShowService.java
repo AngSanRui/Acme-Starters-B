78301122710a9c95f1cbf95cd90789835f638c95
mardomlop11 <58793197+mardomlop11@users.noreply.github.com>
2026-03-09 13:35:51 +0100

    Listado de Sponsorships (S4/2)

---
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipShowService.java
@@ -0,0 +1,45 @@
+
+package acme.features.any.sponsorship;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Any;
+import acme.client.services.AbstractService;
+import acme.entities.sponsorship.Sponsorship;
+
+@Service
+public class AnySponsorshipShowService extends AbstractService<Any, Sponsorship> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AnySponsorshipRepository	repository;
+
+	private Sponsorship					sponsorship;
+
+	// AbstractService interface -------------------------------------------
+
+
+	@Override
+	public void load() {
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		this.sponsorship = this.repository.findSponsorshipById(id);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = this.sponsorship != null && !this.sponsorship.getDraftMode();
+
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.sponsorship, "sponsor", "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode");
+	}
+}
