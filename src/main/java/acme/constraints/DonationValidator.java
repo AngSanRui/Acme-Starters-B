474c65b9bdcab3c6dabcb1aa8c5c5de9ae2e98a3
mardomlop11 <58793197+mardomlop11@users.noreply.github.com>
2026-03-17 12:10:28 +0100

    Corrección comprobación currency EUR en Donation

---
+++ src/main/java/acme/constraints/DonationValidator.java
@@ -0,0 +1,41 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.entities.sponsorship.Donation;
+
+@Validator
+public class DonationValidator extends AbstractValidator<ValidDonation, Donation> {
+
+	// Internal state ---------------------------------------------------------
+
+	// ConstraintValidator interface ------------------------------------------
+
+	@Override
+	protected void initialise(final ValidDonation donation) {
+		assert donation != null;
+	}
+
+	@Override
+	public boolean isValid(final Donation donation, final ConstraintValidatorContext context) {
+		assert context != null;
+
+		boolean result;
+		result = true;
+
+		if (donation == null)
+			result = true;
+		else {
+			boolean noNullValues;
+			boolean supportedCurrency;
+			noNullValues = !(donation.getName().isBlank() || donation.getNotes().isBlank() || donation.getMoney() == null || donation.getKind() == null);
+			supportedCurrency = donation.getMoney().getCurrency().equalsIgnoreCase("EUR");
+			result = noNullValues && supportedCurrency;
+			super.state(context, supportedCurrency, "money", "acme.validation.donation.currency-not-supported.message");
+		}
+		return result;
+	}
+}
