2edf719e7d821a328c1480d3ec09dcfc2f3ef208
Mónica Jingling Núñez Regidor <114425619+QSC6079@users.noreply.github.com>
2026-03-20 13:23:27 +0100

    Corrección de servicios

--- src/main/java/acme/features/spokesperson/campaign/SpokespersonCampaignPublishService.java
+++ src/main/java/acme/features/spokesperson/campaign/SpokespersonCampaignPublishService.java
@@ -58,7 +58,7 @@ public class SpokespersonCampaignPublishService extends AbstractService<Spokespe
 
 	@Override
 	public void unbind() {
-		super.unbindObject(this.campaign, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
+		super.unbindObject(this.campaign, "spokesperson", "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode", "monthsActive", "effort");
 	}
 
 }
